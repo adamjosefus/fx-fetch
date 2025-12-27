4be146083d46912c3c71088240a7329ab82b4bfa
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2025-12-27 09:37:52 +0100

    fix: Implementation of readBodyAsBytesOrThrow fixed. It now returns a Uint8Array, as intended.

--- packages/fx-fetch/src/utils/readBodyAsBytesOrThrow.ts
+++ packages/fx-fetch/src/utils/readBodyAsBytesOrThrow.ts
@@ -2,16 +2,16 @@ import { BodyIsNotPresentError } from '../Cause';
 import { resolveBlobOrThrow } from './resolveBlobOrThrow';
 
 /**
- * Reads the body as bytes (ArrayBuffer), throwing detailed errors if it fails.
+ * Reads the body as bytes (Uint8Array), throwing detailed errors if it fails.
  * @internal
  */
 export async function readBodyAsBytesOrThrow(
   body: Promise<Blob> | undefined
-): Promise<ArrayBuffer> {
+): Promise<Uint8Array<ArrayBuffer>> {
   if (body === undefined) {
     throw new BodyIsNotPresentError({ message: 'Body is not present' });
   }
 
   const blob: Blob = await resolveBlobOrThrow(body);
-  return await blob.arrayBuffer();
+  return await blob.bytes();
 }
