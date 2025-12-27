b491d5a92d903ac1ddc3b39d71a60d1ae7be3158
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2025-12-27 12:04:37 +0100

    docs: add detailed JSDoc example for fetchBlob function

--- packages/fx-fetch/src/Fetch/fetchBlob.ts
+++ packages/fx-fetch/src/Fetch/fetchBlob.ts
@@ -8,6 +8,30 @@ import { fetch } from './fetchFn';
  *
  * @category Functions
  * @since 0.1.0
+ * @see {@link Response.readBlob}
+ * @example
+ * ```ts
+ * import { Effect } from 'effect';
+ * import { Fetch, Request, Response } from 'fx-fetch';
+ *
+ * //       ┌─── Effect.Effect<
+ * //       │      void,
+ * //       │      | Fetch.FetchError
+ * //       │      | Fetch.AbortError
+ * //       │      | Fetch.NotAllowedError
+ * //       │      | Response.NotOkError
+ * //       │      | MalformedBlobError,
+ * //       │      Fetch.Fetch
+ * //       │    >
+ * //       ▼
+ * const program = Effect.gen(function* () {
+ *   const request = Request.unsafeMake({ url: './my-endpoint' });
+ *
+ *   //       ┌─── Blob
+ *   //       ▼
+ *   const payload = yield* Fetch.fetchBlob(request);
+ * });
+ * ```
  */
 export const fetchBlob = (request: Request.Request) =>
   fetch(request).pipe(Effect.flatMap(Response.readBlob));
