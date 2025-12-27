bb831f1e9265de2a3c4b576b6be187a7c2c53f2a
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2025-12-27 11:57:58 +0100

    docs: add detailed JSDoc example for fetchStream function

--- packages/fx-fetch/src/Fetch/fetchStream.ts
+++ packages/fx-fetch/src/Fetch/fetchStream.ts
@@ -8,6 +8,39 @@ import { fetch } from './fetchFn';
  *
  * @category Functions
  * @since 0.1.0
+ * @see {@link Response.readStream}
+ * @example
+ * ```ts
+ * import { Data, Effect } from 'effect';
+ * import { Fetch, Request, Response } from 'fx-fetch';
+ *
+ * class MyError extends Data.TaggedClass('MyError') {}
+ *
+ * //       ┌─── Effect.Effect<
+ * //       │      void,
+ * //       │      | Fetch.FetchError
+ * //       │      | Fetch.AbortError
+ * //       │      | Fetch.NotAllowedError
+ * //       │      | Response.NotOkError
+ * //       │      | MalformedReadableStreamError,
+ * //       │      Fetch.Fetch
+ * //       │    >
+ * //       ▼
+ * const program = Effect.gen(function* () {
+ *   const request = Request.unsafeMake({ url: './my-endpoint' });
+ *
+ *   //       ┌─── Stream<
+ *   //       │      Uint8Array<ArrayBufferLike>,
+ *   //       │      MyError,
+ *   //       │      never
+ *   //       │    >,
+ *   //       ▼
+ *   const stream = yield* Fetch.fetchStream(request, {
+ *     onError: (err) => new MyError(),
+ *     releaseLockOnEnd: true, // optional
+ *   });
+ * });
+ * ```
  */
 export const fetchStream = <E>(request: Request.Request, options: Options<E>) =>
   fetch(request).pipe(Effect.flatMap(readStream(options)));
