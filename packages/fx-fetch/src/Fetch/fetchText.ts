307028593c075f7a6677ecb4bcf1a7a5333fd4ae
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2025-12-27 11:57:22 +0100

    docs: add detailed JSDoc example for fetchText function

--- packages/fx-fetch/src/Fetch/fetchText.ts
+++ packages/fx-fetch/src/Fetch/fetchText.ts
@@ -8,6 +8,30 @@ import { fetch } from './fetchFn';
  *
  * @category Functions
  * @since 0.1.0
+ * @see {@link Response.readText}
+ * @example
+ * ```ts
+ * import { Effect } from 'effect';
+ * import { Fetch, Request, Response } from 'fx-fetch';
+ *
+ * //       ┌─── Effect.Effect<
+ * //       │      void,
+ * //       │      | Fetch.FetchError
+ * //       │      | Fetch.AbortError
+ * //       │      | Fetch.NotAllowedError
+ * //       │      | Response.NotOkError
+ * //       │      | MalformedTextError,
+ * //       │      Fetch.Fetch
+ * //       │    >
+ * //       ▼
+ * const program = Effect.gen(function* () {
+ *   const request = Request.unsafeMake({ url: './my-endpoint' });
+ *
+ *   //       ┌─── string
+ *   //       ▼
+ *   const payload = yield* Fetch.fetchText(request);
+ * });
+ * ```
  */
 export const fetchText = (request: Request.Request) =>
   fetch(request).pipe(Effect.flatMap(Response.readText));
