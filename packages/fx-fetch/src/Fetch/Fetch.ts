7074f19b6830be107c3be97dad33f99b77bf3747
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2026-01-23 15:06:54 +0100

    refactor(Fetch): Refactor Fetch service response types and update example usage

--- packages/fx-fetch/src/Fetch/Fetch.ts
+++ packages/fx-fetch/src/Fetch/Fetch.ts
@@ -4,20 +4,37 @@ import type { Request } from '../Request';
 import { NotOkError, type Response } from '../Response';
 import { AbortError, FetchError, NotAllowedError } from './errors';
 
+export namespace Fetch {
+  /**
+   * Type alias for the successful response type of the Fetch service.
+   * @since 1.2.0
+   * @category Models
+   */
+  export type SuccessType = Response;
+
+  /**
+   * Type alias for the failure response type of the Fetch service.
+   * @since 1.2.0
+   * @category Models
+   */
+  export type ErrorType = AbortError | FetchError | NotAllowedError | NotOkError;
+}
+
 /**
  * Fetch service for making or mocking HTTP requests.
  *
  * @example
- * ```
- * import { Fetch } from "fx-fetch";
+ * ```ts
+ * import { Effect } from 'effect';
+ * import { Fetch, Request } from 'fx-fetch';
  *
  * Effect.gen(function* () {
- *   const request = new Request('https://example.com');
+ *   const request = Request.unsafeMake({ url: 'https://example.com' });
  *
  *   const fetch = yield* Fetch.Fetch; // ◀︎── Get the Fetch service
  *   const response = yield* fetch(request);
  * }).pipe(
- *   Effect.provideService(Fetch.Fetch, Fetch.layer), // ◀︎── Provide built-in Fetch service
+ *   Effect.provide(Fetch.layer), // ◀︎── Provide built-in Fetch layer
  *   Effect.runPromise
  * );
  * ```
@@ -27,7 +44,5 @@ import { AbortError, FetchError, NotAllowedError } from './errors';
  */
 export class Fetch extends Tag('fx-fetch/Fetch')<
   Fetch,
-  (
-    request: Request
-  ) => Effect<Response, AbortError | FetchError | NotAllowedError | NotOkError, never>
+  (request: Request) => Effect<Fetch.SuccessType, Fetch.ErrorType, never>
 >() {}
