4cf85eb7cf1a14309fb8da6d556bbefe15b04d13
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2026-07-11 00:10:04 +0200

    feat: implement searchParamsFromIntermediate function to convert $SearchParams to SearchParams

--- src/core/Url/SearchParams/intermediate/searchParamsFromIntermediate.ts
+++ src/core/Url/SearchParams/intermediate/searchParamsFromIntermediate.ts
@@ -1,4 +1,12 @@
 import type { SearchParams } from '../SearchParams.js';
 import type { $SearchParams } from './$SearchParams.js';
 
-export function searchParamsFromIntermediate(intermediate: $SearchParams): SearchParams {}
+export function searchParamsFromIntermediate(intermediate: $SearchParams): SearchParams {
+  const searchParams = new Map<string, readonly string[]>();
+
+  for (const [key, values] of intermediate) {
+    searchParams.set(key, [...values]);
+  }
+
+  return searchParams;
+}
